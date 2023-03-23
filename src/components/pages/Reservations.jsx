768be6a7b629ccec7c0746bc4c09ddcabfbe6eb1
masouma <masouma2020@gmail.com>
2023-03-23 13:15:29 +0430

    create reservation component

--- src/components/pages/Reservations.jsx
+++ src/components/pages/Reservations.jsx
@@ -1,14 +1,29 @@
 /* eslint-disable */
 import './../pages/styles/reservations.css'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 
-import React from 'react'
+const Reservations = () => {
+    const [reservations, getReservations] = useState([])
+    const { currentUser } = useSelector((state) => state.user)
+    const getReservation = async () => {
+        const response = await fetch(`http://localhost:3000/api/v1/users/${currentUser.data.id}/reservations`);
+        const data = await response.json();
+        getReservations(data.data)
+        return data;
+    };
 
-const Reservations = () => (
-    <div className="reservationsDiv">
-        Reservations
+    useEffect(() => {
+        getReservation()
+    }, [])
 
-    </div>
-)
-
+    return (
+        <div className="reservationsDiv">
+            {reservations.map((reserv) => (
+                <p>{reserv.city}</p>
+            ))}
+        </div>
+    )
+}
 
 export default Reservations
