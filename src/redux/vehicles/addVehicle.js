6b7fb6614d8e1a28ea2d4638e04b6d6d5aebbb5c
Gilbert <okonjogilbert2@gmail.com>
2023-03-30 15:39:47 +0300

    Fix no-param-reassign of  in addVehicle.js

--- src/redux/vehicles/addVehicle.js
+++ src/redux/vehicles/addVehicle.js
@@ -1,24 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 const addVehicleSlice = createSlice({
-    name: "addVehicle",
-    initialState: {
-        isFetching: false,
-        error: false,
-    },
-    reducers: {
-        addStart: (state) => {
-            state.isFetching = true
-        },
-        addSuccess: (state) => {
-            state.isFetching = false
-        },
-        addFailure: (state) => {
-            state.isFetching = false
-            state.error = true
-        },
-    }
-})
+  name: 'addVehicle',
+  initialState: {
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    addStart: (state) => ({ ...state, isFetching: true }),
+    addSuccess: (state) => ({ ...state, isFetching: false }),
+    addFailure: (state) => ({ ...state, isFetching: false, error: true }),
+  },
+});
 
 export const { addStart, addSuccess, addFailure } = addVehicleSlice.actions;
 export default addVehicleSlice.reducer;
