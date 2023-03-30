b802823f0991f53f597e72cb6857f64ee98dc8bc
Gilbert <okonjogilbert2@gmail.com>
2023-03-30 15:31:00 +0300

    Fix no-param-reassign error in userSlice

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -1,30 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
     currentUser: null,
     isFetching: false,
     error: false,
   },
   reducers: {
-    loginStart: (state) => {
-      state.isFetching = true
-    },
+    loginStart: (state) => ({ ...state, isFetching: true }),
     loginSuccess: (state, action) => {
-      state.isFetching = false
-      const token = action.payload
-      localStorage.setItem('token', JSON.stringify(token))
-      const getToken = JSON.parse(localStorage.getItem('token'))
+      const token = action.payload;
+      localStorage.setItem('token', JSON.stringify(token));
+      const getToken = JSON.parse(localStorage.getItem('token'));
 
-      state.currentUser = getToken
+      return { ...state, isFetching: false, currentUser: getToken };
     },
-    loginFailure: (state) => {
-      state.isFetching = false
-      state.error = true
-    },
-  }
-})
+    loginFailure: (state) => ({ ...state, isFetching: false, error: true }),
+  },
+});
 
 export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
 export default userSlice.reducer;
